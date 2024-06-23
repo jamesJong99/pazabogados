9846577ee40e695e04bcf36f5459dd148b18ec51
jamesJong99 <jamesjong99@gmail.com>
2024-06-23 07:52:55 -0700

    removing junk files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,26 +20,10 @@ export default function RootLayout({
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <Script id='fb-pixel' strategy='afterInteractive'>
-          {`!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '2697035570469919');
-          fbq('track', 'PageView');`}
-        </Script>
-        <script src="/js/animaition.js"></script>
         <head />
         <title>PAZ LAWYERS</title>
         <link rel="icon" href="/images/logos/favicon.ico" sizes="any" />
         <body className={outfit.className}>
-          <noscript><img height="1" width="1" style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2697035570469919&ev=PageView&noscript=1"
-          /></noscript>
           <ThemeProvider
             attribute="class"
             defaultTheme="root"
