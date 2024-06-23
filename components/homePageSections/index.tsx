02ee1aa330d227daf759f3fcb41258fe2d48a19b
jamesJong99 <jamesjong99@gmail.com>
2024-06-23 07:48:44 -0700

    removing junk files

--- components/homePageSections/index.tsx
+++ components/homePageSections/index.tsx
@@ -2,16 +2,12 @@
 
 import BannerSection from "./BannerSection";
 import CarouselSection from "./CarouselSection";
-import ProblemSection from "./ProblemSection";
 import ProductionSection from "./ProductionSection";
-import PricingSection from "./PricingSection";
 import FAQSection from "./FAQSection";
 
 export {
     BannerSection,
     CarouselSection,
-    ProblemSection,
     ProductionSection,
-    PricingSection,
     FAQSection
 }
